c07b664cbf72af1496171b1ef889dafef323e2e3
Craig <embracraig@gmail.com>
2020-07-02 13:52:54 +0100

    Added initial migrations

--- LettingAgent/Data/Migrations/20200702124306_InitialCreate.Designer.cs
+++ LettingAgent/Data/Migrations/20200702124306_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using LettingAgent.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LettingAgent.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200702124306_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
